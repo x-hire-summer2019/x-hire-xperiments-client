a57f3985f65122263ffcd51bf810f4910bfb9dc9
Christopher Fox <cfox@xpanxion.com>
2019-09-18 09:10:22 -0400

    new profile stuff added and cards added to contributors

--- src/app/user/profile/user-profile.component.ts
+++ src/app/user/profile/user-profile.component.ts
@@ -2,13 +2,14 @@
 import { Component } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 /* Service imports */
+import { AuthService } from "../../core/services/auth/auth.service";
 
 @Component({
   selector: "app-home",
   templateUrl: "./user-profile.component.html"
 })
 export class UserProfileComponent {
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title, public auth: AuthService) {
     this.titleService.setTitle("X-hire Digital Pods - Profile");
   } // end of constructor
 } // end of class
