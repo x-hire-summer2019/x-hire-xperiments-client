5d03772ee1e4c9f5365f72394310207267850a14
Christopher Fox <cfox@xpanxion.com>
2019-09-27 08:32:10 -0400

    just needs podType

--- src/app/user/profile/user-profile.component.ts
+++ src/app/user/profile/user-profile.component.ts
@@ -1,15 +1,26 @@
 /* Angular imports */
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 /* Service imports */
 import { AuthService } from "../../core/services/auth/auth.service";
+import { PodsService } from "../../core/services/pods/pods.service";
 
 @Component({
   selector: "app-home",
   templateUrl: "./user-profile.component.html"
 })
-export class UserProfileComponent {
-  constructor(private titleService: Title, public auth: AuthService) {
+export class UserProfileComponent implements OnInit {
+  constructor(
+    private titleService: Title,
+    public auth: AuthService,
+    public podsService: PodsService
+  ) {
     this.titleService.setTitle("X-hire Digital Pods - Profile");
   } // end of constructor
+
+  ngOnInit() {
+    if (!this.podsService.singlePod) {
+      this.podsService.singlePod = this.podsService.allPods[0];
+    }
+  }
 } // end of class
